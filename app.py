dc1aa8bba2e3c5bfd364228512124c6274f9aa06
Nida-Fathima2004 <156936001+Nida-Fathima2004@users.noreply.github.com>
2025-11-20 12:30:39 +0530

    Update app.py

--- app.py
+++ app.py
@@ -2,11 +2,11 @@ import streamlit as st
 from transformers import AutoModelForObjectDetection, AutoImageProcessor
 from PIL import Image
 import numpy as np
-import cv2
 
 st.title("🚗 Vehicle Detection (Hugging Face Model)")
 
 MODEL_ID = "keremberke/yolov8s-vehicles"
+
 
 @st.cache_resource
 def load_model():
@@ -25,20 +25,21 @@ if img:
     outputs = model(**inputs)
 
     result = processor.post_process_object_detection(
-        outputs,
-        threshold=0.3,
+        outputs, threshold=0.4,
         target_sizes=[image.size[::-1]]
     )[0]
 
-    # Convert to NumPy
     image_np = np.array(image)
 
-    # Draw boxes
+    # Draw boxes manually
+    import cv2
+
     for score, label, box in zip(result["scores"], result["labels"], result["boxes"]):
         x1, y1, x2, y2 = map(int, box.tolist())
         name = model.config.id2label[label.item()]
+
         cv2.rectangle(image_np, (x1, y1), (x2, y2), (0,255,0), 2)
         cv2.putText(image_np, f"{name} {float(score):.2f}", (x1, y1 - 10),
-                    cv2.FONT_HERSHEY_SIMPLEX, 0.7, (0,255,0), 2)
+                    cv2.FONT_HERSHEY_SIMPLEX, 0.6, (0,255,0), 2)
 
-    st.image(image_np, caption="Vehicle Detection Output")
+    st.image(image_np, caption="Detected Vehicles")
